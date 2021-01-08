23b89d4868a2901ba1b09a40b7e908eddeba934d
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2021-01-08 09:12:05 +0530

    adding Task 2 Code

---
+++ 101_Challenge Tasks/Task 2/helper.py
@@ -0,0 +1,30 @@
+import cv2
+import glob
+import time
+
+
+def convert_to_rgb(img):
+    time.sleep(0.0002)
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def resize_image(img):
+    time.sleep(0.0002)
+    return cv2.resize(img, (400, 400))
+
+
+def convert_to_gray(img):
+    time.sleep(0.0002)
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+def missing_frames(directory, frame_count=80):
+    processed_frames = glob.glob(r'{}//*.jpg'.format(directory))
+    processed_frames_numbers = []
+    for filename in processed_frames:
+        file_no_ext = filename.split("/")[-1]
+        file_number = file_no_ext.split('.')[-2]
+        processed_frames_numbers.append(int(file_number))
+
+    frame_no_list = [x for x in range(1, frame_count+1)]
+    return [x for x in frame_no_list if x not in processed_frames_numbers]
---
+++ 101_Challenge Tasks/Task 2/video_capture.py
@@ -0,0 +1,57 @@
+import time
+from queue import Queue
+from threading import Thread
+
+import cv2
+
+
+class VideoReader:
+
+    initialized = False
+
+    def __init__(self, video_path, queue_size=512):
+        self.stream = cv2.VideoCapture(video_path)
+        self.q = Queue(maxsize=queue_size)
+        self.initialized = True
+        self.complete = False
+
+    def start(self):
+
+        if self.initialized:
+            t = Thread(target=self.__read_frames, args=())
+            t.daemon = True
+            t.start()
+
+        else:
+            raise Exception
+
+    def __read_frames(self):
+
+        while self.stream.isOpened():
+
+            if not self.q.full():
+
+                grabbed, frame = self.stream.read()
+
+                if not grabbed:
+                    self.complete = True
+                    return
+
+                self.q.put(frame)
+
+            time.sleep(0.001)
+
+    def get_video_details(self):
+
+        width = int(self.stream.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = self.stream.get(cv2.CAP_PROP_FPS)
+        frame_count = int(self.stream.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        return width, height, fps, frame_count
+
+    def more(self):
+        return not self.complete
+
+    def read(self):
+        return self.q.get()

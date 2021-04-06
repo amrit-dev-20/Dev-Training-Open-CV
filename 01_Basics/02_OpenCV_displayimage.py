a3bafd24a4931244e9b93ebb33098e662fbe15e6
amrit2356 <amritanshupandey.meritocrat@gmail.com>
2021-04-06 12:05:42 +0530

    Code updated

--- 01_Basics/02_OpenCV_displayimage.py
+++ 01_Basics/02_OpenCV_displayimage.py
@@ -1,10 +1,8 @@
 import cv2
 
-path ='images/beautiful-water-drop-on-dandelion-260nw-789676552.jpg'
+path = 'images/beautiful-water-drop-on-dandelion-260nw-789676552.jpg'
 window_name = "Test Window to Display Images"
 image = cv2.imread(path)
-cv2.imshow(window_name,image)
+cv2.imshow(window_name, image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
